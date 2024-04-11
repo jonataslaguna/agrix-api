6767ba3e28897bb6dd2965bdb812b26a4dfdc805
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-04-11 13:50:47 -0300

    implemented validateToken in TokenService

--- src/main/java/com/betrybe/agrix/ebytr/staff/service/TokenService.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/service/TokenService.java
@@ -36,4 +36,17 @@ public class TokenService {
     return Instant.now()
         .plus(2, ChronoUnit.HOURS);
   }
+
+  /**
+   * Validate token string.
+   *
+   * @param token the token
+   * @return the string
+   */
+  public String validateToken(String token) {
+    return JWT.require(algorithm)
+        .build()
+        .verify(token)
+        .getSubject();
+  }
 }

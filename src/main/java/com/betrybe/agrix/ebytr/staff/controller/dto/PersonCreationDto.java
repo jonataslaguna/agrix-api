c48280dc1303180dc81a04c8ca004daeef769bdd
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-04-11 11:55:33 -0300

    implemented PersonCreationDto

---
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controller/dto/PersonCreationDto.java
@@ -0,0 +1,18 @@
+package com.betrybe.agrix.ebytr.staff.controller.dto;
+
+import com.betrybe.agrix.ebytr.staff.entity.Person;
+import com.betrybe.agrix.ebytr.staff.security.Role;
+
+/**
+ * The type Person creation dto.
+ */
+public record PersonCreationDto(
+    String username,
+    String password,
+    Role role
+) {
+
+  public Person toEntity() {
+    return new Person(null, username, password, role);
+  }
+}

b7de6bb9e354561b7379943d94fbc9ac4079d0ac
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-04-11 09:38:01 -0300

    entity Farm

---
+++ src/main/java/com/betrybe/agrix/ebytr/staff/entity/Farm.java
@@ -0,0 +1,65 @@
+package com.betrybe.agrix.ebytr.staff.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+
+/**
+ * The type Farm.
+ */
+@Entity
+@Table(name = "farm")
+public class Farm {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private double size;
+  @OneToMany(mappedBy = "farm")
+  private List<Crop> crops;
+
+  public Farm() {
+  }
+
+  public Farm(String name, double size) {
+    this.name = name;
+    this.size = size;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getSize() {
+    return size;
+  }
+
+  public void setSize(double size) {
+    this.size = size;
+  }
+
+  public List<Crop> getCrops() {
+    return crops;
+  }
+
+  public void setCrops(List<Crop> crops) {
+    this.crops = crops;
+  }
+}
+

e9151add90553ba801c7af1c330a16d26cb6254e
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-04-11 09:51:41 -0300

    GlobalControllerAdvice

---
+++ src/main/java/com/betrybe/agrix/ebytr/staff/advice/GlobalControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.betrybe.agrix.ebytr.staff.advice;
+
+import com.betrybe.agrix.ebytr.staff.service.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * The type Global controller advice.
+ */
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+  /**
+   * Handle not found response entity.
+   *
+   * @param exception the exception
+   * @return the response entity
+   */
+  @ExceptionHandler
+  public ResponseEntity<String> handleNotFound(NotFoundException exception) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(exception.getMessage());
+  }
+}
+

236cbe4e35dae9647345ad51280d2003394907f5
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-04-25 15:25:02 -0300

    organizing exceptions files

--- src/main/java/com/betrybe/agrix/ebytr/staff/service/exception/FarmNotFoundException.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/service/exception/FarmNotFoundException.java
@@ -7,4 +7,15 @@ public class FarmNotFoundException extends NotFoundException {
   public FarmNotFoundException() {
     super("Farm not found");
   }
+
+  /**
+   * Exception for when a person is not found.
+   */
+  public static class PersonNotFoundException extends RuntimeException {
+
+    public PersonNotFoundException() {
+      super("Person not found");
+    }
+
+  }
 }
